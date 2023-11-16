1a6367828d765c3727e962b8aee02786bbb27c08
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2023-11-16 16:16:39 +0530

    Customer Reports added

--- src/main/java/com/example/jasper/controller/CustomerReportsFormController.java
+++ src/main/java/com/example/jasper/controller/CustomerReportsFormController.java
@@ -1,11 +1,18 @@
 package com.example.jasper.controller;
 
+import com.example.jasper.db.DbConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 
 public class CustomerReportsFormController {
 
@@ -27,8 +34,19 @@ public class CustomerReportsFormController {
 
 
     @FXML
-    void printCustomerListOnAction(ActionEvent event) {
+    void printCustomerListOnAction(ActionEvent event) throws JRException, SQLException {
+        InputStream resourceAsStream = getClass().getResourceAsStream("../report/CustomerList2.jrxml");
+        JasperDesign load = JRXmlLoader.load(resourceAsStream);
+        JasperReport jasperReport = JasperCompileManager.compileReport(load);
 
+        JasperPrint jasperPrint =
+                JasperFillManager.fillReport(
+                        jasperReport, //compiled report
+                        null,
+                        DbConnection.getInstance().getConnection() //database connection
+                );
+
+        JasperViewer.viewReport(jasperPrint, false);
     }
 
 }
